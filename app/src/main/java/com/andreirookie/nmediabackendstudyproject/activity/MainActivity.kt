3e3b7d7ce0deb38b821bf27a6ac2803407fa0e6a
andreiRookie <andrey.idem@gmail.com>
2022-11-07 11:50:47 +0400

    avatars images loading via Glide

--- app/src/main/java/com/andreirookie/nmediabackendstudyproject/activity/MainActivity.kt
+++ app/src/main/java/com/andreirookie/nmediabackendstudyproject/activity/MainActivity.kt
@@ -7,13 +7,17 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
+import com.andreirookie.nmediabackendstudyproject.R
 import com.andreirookie.nmediabackendstudyproject.databinding.ActivityMainBinding
+import com.bumptech.glide.Glide
 import okhttp3.*
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
-// Second vers: метод не оч хороший, размер сообщения ограничен ~ 1Mb
-// в Bundle'ы картинки записывать нельзя
+
+
+// Third version: via Glide
+
 class MainActivity : AppCompatActivity() {
 
     private val urls = listOf("netology.jpg", "sber.jpg", "tcs.jpg", "404.png")
@@ -30,61 +34,107 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val handler = Handler(Looper.getMainLooper()) { message ->
-            //demo
-            val bitmap = message.data["image"] as Bitmap?
-            bitmap.let { binding.image.setImageBitmap(bitmap)}
-            println("Handler(Looper.getMainLooper()){}: image set")
-
-            return@Handler true
-        }
-
         binding.load.setOnClickListener {
-            if (index == urls.size) {
-                index = 0
-            }
-
+            if (index == urls.size) index = 0
 
             val url = "http://10.0.2.2:9999/avatars/${urls[index++]}"
             println("Loading: $url")
-            // запрос на сервер
-            val request = Request.Builder()
-                .url(url)
-                .build()
 
-            client.newCall(request)
-           // тут из главного уходим в не фоновый поток через enqueue()
-                .enqueue(object : Callback {
-                    override fun onResponse(call: Call, response: Response) {
-                        response.body?.use { responseBody ->
+            //указываем Context, с которым работаем
+          //  Glide.with(binding.image) - из view сам вытащит контекст
+            Glide.with(this)
+                .load(url) // передаем адрес картинки
+                .placeholder(R.drawable.ic_loading_100dp) //пикча в момент загрузки
+                .error(R.drawable.ic_error_100dp)
+                // glide по умолчанию не ждёт, поэтому нужно прописать timeout
+                .timeout(10_000)
+                .into(binding.image)
 
-                            println("loaded: $url")
-                            println(responseBody.contentType())
-                            println(responseBody.contentLength())
+                //картинки кешируются
 
-                            val bitmap = BitmapFactory.decodeStream(responseBody.byteStream())
-
-                            // более простой способ отправить в Uithread
-//                            this@MainActivity.runOnUiThread {
-//                                bitmap?.let {binding.image.setImageBitmap(bitmap)}
-//                                println("this@MainActivity.runOnUiThread: image set")
-//                            }
-
-                            val message = handler.obtainMessage().apply {
-                                data = bundleOf("image" to bitmap)
-                            }
-                            //отправляем картинку в handler главного потока
-                            handler.sendMessage(message)
-                        }
-                    }
-
-                    override fun onFailure(call: Call, e: IOException) {
-                        e.printStackTrace()
-                    }
-                })
         }
+
     }
+
 }
+
+// Second vers: метод не оч хороший, размер сообщения ограничен ~ 1Mb
+// в Bundle'ы картинки записывать нельзя
+//class MainActivity : AppCompatActivity() {
+//
+//    private val urls = listOf("netology.jpg", "sber.jpg", "tcs.jpg", "404.png")
+//    private var index = 0
+//
+//    private val client = OkHttpClient.Builder()
+//        .connectTimeout(10,TimeUnit.SECONDS)
+//        .build()
+//
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        val binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        // handler для looper'а главного потока
+//        val handler = Handler(Looper.getMainLooper()) { message ->
+//            //demo
+//            val bitmap = message.data["image"] as Bitmap?
+//            bitmap.let { binding.image.setImageBitmap(bitmap)}
+//            println("Handler(Looper.getMainLooper()){}: image set")
+//
+//            return@Handler true
+//        }
+//
+//        binding.load.setOnClickListener {
+//            if (index == urls.size) {
+//                index = 0
+//            }
+//
+//
+//            val url = "http://10.0.2.2:9999/avatars/${urls[index++]}"
+//            println("Loading: $url")
+//            // запрос на сервер
+//            val request = Request.Builder()
+//                .url(url)
+//                .build()
+//
+//            client.newCall(request)
+//           // тут из главного уходим в фоновый поток через enqueue()
+//                .enqueue(object : Callback {
+//                    override fun onResponse(call: Call, response: Response) {
+//                        response.body?.use { responseBody ->
+//
+//                            println("loaded: $url")
+//                            println(responseBody.contentType())
+//                            println(responseBody.contentLength())
+//
+//                            val bitmap = BitmapFactory.decodeStream(responseBody.byteStream())
+//
+//                            // более простой способ отправить в UiThread
+////                            this@MainActivity.runOnUiThread {
+////                                bitmap?.let {binding.image.setImageBitmap(bitmap)}
+////                                println("this@MainActivity.runOnUiThread: image set")
+////                            }
+//
+//                            val message = handler.obtainMessage().apply {
+//                                data = bundleOf("image" to bitmap)
+//                            }
+//                            //отправляем картинку в handler нашей activity, главный поток
+//                            handler.sendMessage(message)
+//                        }
+//                    }
+//
+//                    override fun onFailure(call: Call, e: IOException) {
+//                        e.printStackTrace()
+//                    }
+//                })
+//        }
+//    }
+//}
+
+
+
 /*  First version: getting images without imageView bindings
 
 class MainActivity : AppCompatActivity() {
